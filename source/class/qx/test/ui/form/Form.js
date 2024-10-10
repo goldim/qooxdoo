02eab8d076c169a67d7aa4038e3edffa207e27b3
ZolotovDY <ZolotovDY@ugpa.ru>
2024-10-10 15:38:00 +0300

    Return test and

--- source/class/qx/test/ui/form/Form.js
+++ source/class/qx/test/ui/form/Form.js
@@ -427,6 +427,21 @@ qx.Class.define("qx.test.ui.form.Form", {
 
       cont.dispose();
       rb.destroy();
+    },
+
+    testRedefineItem() {
+      var form = new qx.ui.form.Form();
+      var resetter = form._resetter;
+      resetter.redefineItem = this.spy(resetter.redefineItem);
+
+      var item = new qx.ui.form.TextField();
+      form.add(item, "xyz");
+      form.redefineResetterItem(item);
+
+      this.assertCalledOnce(resetter.redefineItem);
+
+      item.dispose();
+      form.dispose();
     }
   }
 });
