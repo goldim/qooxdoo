5fabca3f7b98538355bed109d2433a453ccfd27d
ZolotovDY <ZolotovDY@ugpa.ru>
2024-10-10 14:51:42 +0300

    Log hide method in widget

--- source/class/qx/ui/core/Widget.js
+++ source/class/qx/ui/core/Widget.js
@@ -2131,6 +2131,7 @@ qx.Class.define("qx.ui.core.Widget", {
       if (value === "visible") {
         content.show();
       } else {
+        console.log("widget hide", content);
         content.hide();
       } // only force a layout update if visibility change from/to "exclude"
       var parent = this.$$parent;
